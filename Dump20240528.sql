3960b152c77676cda8a073126bc5b8aefa2a3874
Daniel Vilar Martínez <145765230+eldaniii@users.noreply.github.com>
2024-05-27 12:21:09 +0200

    miproyecto-schema.sql

--- Dump20240528.sql
+++ Dump20240528.sql
@@ -35,25 +35,6 @@ CREATE TABLE `campeon` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Temporary view structure for view `detallescampeonpartido11`
---
-
-DROP TABLE IF EXISTS `detallescampeonpartido11`;
-/*!50001 DROP VIEW IF EXISTS `detallescampeonpartido11`*/;
-SET @saved_cs_client     = @@character_set_client;
-/*!50503 SET character_set_client = utf8mb4 */;
-/*!50001 CREATE VIEW `detallescampeonpartido11` AS SELECT 
- 1 AS `equipo_nombre`,
- 1 AS `tipo`,
- 1 AS `campeon_nombre`,
- 1 AS `habilidad1`,
- 1 AS `habilidad2`,
- 1 AS `habilidad3`,
- 1 AS `habilidad4`,
- 1 AS `pasiva`*/;
-SET character_set_client = @saved_cs_client;
-
---
 -- Table structure for table `equipo`
 --
 
@@ -182,18 +163,6 @@ CREATE TABLE `ronda_has_campeon` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Temporary view structure for view `topequipos2018`
---
-
-DROP TABLE IF EXISTS `topequipos2018`;
-/*!50001 DROP VIEW IF EXISTS `topequipos2018`*/;
-SET @saved_cs_client     = @@character_set_client;
-/*!50503 SET character_set_client = utf8mb4 */;
-/*!50001 CREATE VIEW `topequipos2018` AS SELECT 
- 1 AS `nombre`*/;
-SET character_set_client = @saved_cs_client;
-
---
 -- Table structure for table `torneo`
 --
 
@@ -210,50 +179,5 @@ CREATE TABLE `torneo` (
   PRIMARY KEY (`ano`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Final view structure for view `detallescampeonpartido11`
---
-
-/*!50001 DROP VIEW IF EXISTS `detallescampeonpartido11`*/;
-/*!50001 SET @saved_cs_client          = @@character_set_client */;
-/*!50001 SET @saved_cs_results         = @@character_set_results */;
-/*!50001 SET @saved_col_connection     = @@collation_connection */;
-/*!50001 SET character_set_client      = utf8mb4 */;
-/*!50001 SET character_set_results     = utf8mb4 */;
-/*!50001 SET collation_connection      = utf8mb4_0900_ai_ci */;
-/*!50001 CREATE ALGORITHM=UNDEFINED */
-/*!50013 DEFINER=`root`@`localhost` SQL SECURITY DEFINER */
-/*!50001 VIEW `detallescampeonpartido11` AS select `e`.`nombre` AS `equipo_nombre`,`rhc`.`tipo` AS `tipo`,`c`.`nombre` AS `campeon_nombre`,`c`.`habilidad1` AS `habilidad1`,`c`.`habilidad2` AS `habilidad2`,`c`.`habilidad3` AS `habilidad3`,`c`.`habilidad4` AS `habilidad4`,`c`.`pasiva` AS `pasiva` from (((`campeon` `c` join `ronda_has_campeon` `rhc` on((`c`.`idCAMPEON` = `rhc`.`CAMPEON_idCAMPEON`))) join `partido` `p` on(((`rhc`.`EQUIPO_idEQUIPO` = `p`.`EQUIPO_idEQUIPO_rojo`) or (`rhc`.`EQUIPO_idEQUIPO` = `p`.`EQUIPO_idEQUIPO_azul`)))) join `equipo` `e` on((`rhc`.`EQUIPO_idEQUIPO` = `e`.`idEQUIPO`))) where (`p`.`idPARTIDO` = 11) */;
-/*!50001 SET character_set_client      = @saved_cs_client */;
-/*!50001 SET character_set_results     = @saved_cs_results */;
-/*!50001 SET collation_connection      = @saved_col_connection */;
-
---
--- Final view structure for view `topequipos2018`
---
-
-/*!50001 DROP VIEW IF EXISTS `topequipos2018`*/;
-/*!50001 SET @saved_cs_client          = @@character_set_client */;
-/*!50001 SET @saved_cs_results         = @@character_set_results */;
-/*!50001 SET @saved_col_connection     = @@collation_connection */;
-/*!50001 SET character_set_client      = utf8mb4 */;
-/*!50001 SET character_set_results     = utf8mb4 */;
-/*!50001 SET collation_connection      = utf8mb4_0900_ai_ci */;
-/*!50001 CREATE ALGORITHM=UNDEFINED */
-/*!50013 DEFINER=`root`@`localhost` SQL SECURITY DEFINER */
-/*!50001 VIEW `topequipos2018` AS select `e`.`nombre` AS `nombre` from ((`equipo` `e` join `ranking` `r` on((`e`.`idEQUIPO` = `r`.`EQUIPO_idEQUIPO`))) join `torneo` `t` on((`r`.`TORNEO_ano` = `t`.`ano`))) where (`r`.`TORNEO_ano` = 2018) order by `r`.`posicion` desc limit 10 */;
-/*!50001 SET character_set_client      = @saved_cs_client */;
-/*!50001 SET character_set_results     = @saved_cs_results */;
-/*!50001 SET collation_connection      = @saved_col_connection */;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
 -- Dump completed on 2024-05-27  8:47:21
